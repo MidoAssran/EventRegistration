23989d8ee7fac40b31c38342f105576968a063a8
MidoAssran <midostuf@hotmail.com>
2016-01-27 00:41:48 -0500

    ADDED - Working Persistence!

--- workspace-mobile/EventRegistration/app/src/main/java/ca/mcgill/ecse321/eventregistration/MainActivity.java
+++ workspace-mobile/EventRegistration/app/src/main/java/ca/mcgill/ecse321/eventregistration/MainActivity.java
@@ -5,15 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationController;
 import ca.mcgill.ecse321.eventregistration.controller.InvalidInputException;
+import ca.mcgill.ecse321.eventregistration.model.Participant;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+import ca.mcgill.ecse321.eventregistration.persistence.PersistenceEventRegistration;
 
 public class MainActivity extends AppCompatActivity {
+
+    RegistrationManager rm;
+    HashMap<Integer, Participant> participants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +43,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Log.i("Creation", getFilesDir().getAbsolutePath() + "eventregistration.xml");
+        PersistenceEventRegistration.setFilename(getFilesDir().getAbsolutePath() + "eventregistration.xml");
+
+        // load model
+        PersistenceEventRegistration.loadEventRegistrationModel();
+        rm = RegistrationManager.getInstance();
         refreshData();
 
     }
 
     public void refreshData() {
+        // Initialize the data in the participant spinner
+        Spinner spinner = (Spinner) findViewById(R.id.participantspinner);
+
+        ArrayAdapter<CharSequence> participantAdapter = new
+                ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+
+        participantAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        this.participants = new HashMap<Integer, Participant>();
+        int i = 0;
+        for (Iterator<Participant> participants = rm.getParticipants().iterator();
+             participants.hasNext(); i++) {
+            Participant p = participants.next();
+            participantAdapter.add(p.getName());
+            this.participants.put(i, p);
+        }
+
+        spinner.setAdapter(participantAdapter);
         TextView tv = (TextView) findViewById(R.id.newparticipant_name);
         tv.setText("");
     }
@@ -63,12 +99,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addParticipant(View v) {
+        Log.i("Add Participant", "I'm here Mido");
         TextView tv = (TextView) findViewById(R.id.newparticipant_name);
         EventRegistrationController pc = new EventRegistrationController();
         try {
             pc.createParticipant(tv.getText().toString());
         } catch (InvalidInputException e) {
-
+            Log.e("Add Participant Error", e.getMessage());
         }
 
         refreshData();
