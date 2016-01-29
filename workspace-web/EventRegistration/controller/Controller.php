cb1487fe892ad07a0adb02942c952d138b15c515
MidoAssran <midostuf@hotmail.com>
2016-01-29 05:09:24 -0500

    FIXED - Controller to pass PHPUnit tests

--- workspace-web/EventRegistration/controller/Controller.php
+++ workspace-web/EventRegistration/controller/Controller.php
@@ -1,10 +1,10 @@
 <?php
-require_once "controller/InputValidator.php";
-require_once "persistence/PersistenceEventRegistration.php";
-require_once "model/Participant.php";
-require_once "model/Event.php";
-require_once "model/Registration.php";
-require_once "model/RegistrationManager.php";
+require_once __DIR__.'/../controller/InputValidator.php';
+require_once __DIR__.'/../persistence/PersistenceEventRegistration.php';
+require_once __DIR__.'/../model/Participant.php';
+require_once __DIR__.'/../model/Event.php';
+require_once __DIR__.'/../model/Registration.php';
+require_once __DIR__.'/../model/RegistrationManager.php';
 
 class Controller
 {
@@ -40,16 +40,24 @@ class Controller
 		date('H:i', strtotime($starttime));
 		date('H:i', strtotime($endtime));
 
+		$error = "";
 		// Check Validity
-		if ($name == null || strlen($name) == 0) {
-			throw new Exception("Event name cannot be empty!");
-		} elseif (strtotime($event_date) == false) {
-			throw new Exception("Invalid Date!");
-		} elseif (strtotime($starttime) == false) {
-			throw new Exception("Invalid Start Time!");
-		} elseif (strtotime($endtime) == false) {
-			throw new Exception("Invalid End Time!");
-		} else {
+		if ($name == null || strlen(preg_replace('/\s+/', '', $name)) == 0) {
+			$error = $error . "@1Event name cannot be empty! ";
+		} 
+		if (strtotime(preg_replace('/\s+/', '', $event_date)) == false) {
+			$error = $error . "@2Event date must be specified correctly (YYYY-MM-DD)! ";				
+		} 
+		if (strtotime(preg_replace('/\s+/', '', $starttime)) == false) {
+			$error = $error .  "@3Event start time must be specified correctly (HH:MM)! ";
+		} 
+		if (strtotime(preg_replace('/\s+/', '', $endtime)) == false) {
+			$error = $error .  "@4Event end time must be specified correctly (HH:MM)!";
+		} 
+		if (strtotime($endtime) < strtotime($starttime)) {
+			$error = $error .  "@4Event end time cannot be before event start time!";
+		} 
+		if (strlen($error) == 0) {
 			// 2. Load all of the data
 			$pm = new PersistenceEventRegistration();
 			$rm = $pm->loadDataFromStore();
@@ -60,6 +68,8 @@ class Controller
 
 			// 4. Write all of the data
 			$pm->writeDataToStore($rm);
+		} else {
+			throw new Exception($error);
 		}
 	}
 	
